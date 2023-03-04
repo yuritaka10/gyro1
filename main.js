ddb4a8908e773d8e36901eaf49945301058aae24
Gaga_riiiiiiin <80557945+yuritaka10@users.noreply.github.com>
2023-03-05 08:56:45 +0900

    edit icon link

---
+++ main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+registSW();
+getPost();
+
+function registSW() {
+
+  // Service Worker 対応ブラウザの場合、スコープに基づいてService Worker を登録する
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+      navigator.serviceWorker.register('./sw.js', { scope: './' }).then(function (registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function (err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+}
+
+function getPost() {
+
+  fetch('https://qiita.com/api/v2/items')
+    .then(response => {
+      return response.json();
+
+    }).then(res => {
+
+      const title = res[0].title;
+      const url = res[0].url;
+      const data = `<a href="${url}">${title}</a>`;
+      document.getElementById("newitem").innerHTML = data;
+
+    }).catch(function (error) {
+      console.log(error);
+    });
+}
+
--- sw.js
+++ sw.js
@@ -1,3 +1,59 @@
-self.addEventListener('fetch', function(e) {
 
-})
+//https://qiita.com/TakeshiNickOsanai/items/8d012a128827c9db980d
+
+// Service Worker のバージョンとキャッシュする App Shell を定義する
+
+const NAME = 'qiita-post-app-v2-';
+const VERSION = '002';
+const CACHE_NAME = NAME + VERSION;
+const urlsToCache = [
+  './index.html',
+  './main.js',
+];
+
+// Service Worker へファイルをインストール
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// リクエストされたファイルが Service Worker にキャッシュされている場合
+// キャッシュからレスポンスを返す
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin')
+    return;
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Cache Storage にキャッシュされているサービスワーカーのkeyに変更があった場合
+// 新バージョンをインストール後、旧バージョンのキャッシュを削除する
+// (このファイルでは CACHE_NAME をkeyの値とみなし、変更を検知している)
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!CACHE_NAME.includes(key)) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log(CACHE_NAME + "activated");
+    })
+  );
+});
