e82f9306102c5aeb3c70dbb5091375f549ab2b22
Gaga_riiiiiiin <80557945+yuritaka10@users.noreply.github.com>
2023-03-05 14:06:09 +0900

    shuusei url

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function registSW() {
   // Service Worker 対応ブラウザの場合、スコープに基づいてService Worker を登録する
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-      navigator.serviceWorker.register('sw.js', { scope: '' }).then(function (registration) {
+      navigator.serviceWorker.register('sw.js', { scope: '.' }).then(function (registration) {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function (err) {
         console.log('ServiceWorker registration failed: ', err);
@@ -19,7 +19,7 @@ function registSW() {
 
 function getPost() {
 
-  fetch('https://qiita.com/api/v2/items')
+  fetch('https://yuritaka10.github.io/gyro1/')
     .then(response => {
       return response.json();
 
